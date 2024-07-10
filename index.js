fc9469428441c25b8a276ca0fef85af684e8e8a1
alvin <alvin.sarisar@student.moringaschool.com>
2024-07-10 16:01:36 +0300

    loop assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+let myArray = []
+function writeCards(names){
+  let messages =[] ;
+ for(let i=0;i<names.length;i++){
+  let message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+  messages.push(message);
+debugger;
+}
+return messages; 
+}
+myArray =writeCards(names);
+console.log(myArray)
+
+function countdown (){
+  let i=10 ; 
+  while(i>0 ){console.log(i--)}
+}
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number--);
+  
+  }
+}
+
+countDown(10);
